e28e683d9f5e9ac0737de0a1920f0d788f4e2b0f
Lance Ennen <byennen@gmail.com>
2014-04-23 11:27:54 -0500

    update image sizes

--- app/uploaders/blog/image_uploader.rb
+++ app/uploaders/blog/image_uploader.rb
@@ -2,16 +2,16 @@ class Blog::ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   include CarrierWave::MiniMagick
 
-  process :convert => 'png'
+  process :convert => 'jpg'
   process :tags => ['post_picture']
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_fit => [358, 269]
+     process :resize_to_fill => [702, 390]
   end
 
   version :full_size do
-    process :resize_to_fit => [770, 450]
+    process :resize_to_fill => [770, 450]
   end
 
 end
